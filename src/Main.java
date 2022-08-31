0b899016440e1483285af2f610b999d2eb47a85c
Rylan Hughey <100142122@lubbockisd.net>
2022-08-31 10:12:22 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hi");
+        System.out.println("How are you?");
+    }
+}
+
